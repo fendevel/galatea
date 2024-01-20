a4284fb50e5c814801a68fb28de2bc5fa326a9d3
Jack Fenech <jackpfenech@outlook.com>
2024-01-20 23:41:23 +0000

    Add "Banana" star mode

--- source/main.ts
+++ source/main.ts
@@ -1,6 +1,7 @@
 import { APIPingInteraction, Application, ApplicationCommandOptionType, ApplicationCommandType, AttachmentBuilder, ChannelType, Client, Collection, CommandInteraction, ContextMenuCommandBuilder, Events, GatewayIntentBits, Guild, GuildMember, Interaction, InteractionResponse, MessageContextMenuCommandInteraction, MessagePayload, REST, Routes, SlashCommandBuilder, SlashCommandMentionableOption, SlashCommandUserOption, Snowflake, User, UserContextMenuCommandInteraction, VoiceBasedChannel, VoiceChannel } from "discord.js"
 const { token, clientId, testGuildId } = require("../config.json")
-import {Canvas, loadImage, registerFont} from "canvas"
+// import {Canvas, loadImage, FontLibrary, DOMMatrix, Path2D} from "skia-canvas"
+import {Canvas, loadImage, registerFont, DOMMatrix} from "canvas"
 import * as path from "node:path"
 import * as fs from "node:fs"
 
@@ -78,12 +79,18 @@ commands.set("award", {
             const text = interaction.options.getString("text")
             const count = interaction.options.getInteger("count")
 
-            if (Math.abs(count) > 10000) {
+            if (count > 10000) {
                 interaction.reply("Yeah I'm not drawing that.")
                 return
             }
 
-            const buffer = await drawTriedStar(text ? text : pickLine(domain), count)
+            let buffer: Buffer = undefined
+            if (text && text == "banana") {
+                buffer = await drawBanana(count)
+            } else {
+                buffer = await drawTriedStar(text ? text : pickLine(domain), count)
+            }
+
             const result = await interaction.reply({
                 content: user ? `${user}` : undefined,
                 files: [new AttachmentBuilder(buffer)]
@@ -201,14 +208,15 @@ async function drawTriedStar(chosenLine: string, count: number | undefined = und
     const w = 1024
     const h = 1024
 
-    const canvas = new Canvas(w, h, "image")
+    const canvas = new Canvas(w, h)
     const ctx = canvas.getContext("2d")
+
     registerFont("data/COMIC.TTF", {
         family: "Comic Sans",
     })
 
-    ctx.patternQuality = "best"
     ctx.quality = "best"
+    ctx.patternQuality = "best"
 
     const starFillColour = "#D7B144"
     const starFillColourBright = "#F4D679"
@@ -309,7 +317,236 @@ async function drawTriedStar(chosenLine: string, count: number | undefined = und
     }
 
 
-    return canvas.toBuffer()
+    return canvas.toBuffer("image/png")
+}
+
+async function drawBanana(count: number | undefined = undefined): Promise<Buffer> {
+    const w = 1024
+    const h = 1024
+
+    const canvas = new Canvas(w, h)
+    const ctx = canvas.getContext("2d")
+    // const image = await loadImage("./data/venus vector.svg")
+
+    const image_size = 1080
+
+    ctx.quality = "best"
+    ctx.patternQuality = "best"
+
+    const cx = w/2
+    const cy = h/2
+    const radius = w/2
+    const bananaRadius = w/4
+
+    const armBias = [100, 12, 9, 8, 8, 8, 7, 7, 6, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 4, 4, 3, 3, 2, 2]
+    const armCount = count != undefined ? count : armBias[Math.round(Math.random()*(armBias.length - 1))]
+    const orbitAmount = (Math.PI*2)/(armCount*2)
+
+    let originalInner = bananaRadius, originalOuter = radius*1.0
+    let dx = 0, dy = 1
+
+    // ctx.fill("evenodd")
+    // ctx.clip("evenodd")
+    ctx.lineCap = "round"
+    ctx.lineJoin = "round"
+    ctx.miterLimit = 1.5
+
+    ctx.fillStyle = "#fd0"
+
+    if (armCount < 0) {
+        ctx.fillRect(0, 0, w, h)
+        ctx.globalCompositeOperation = "destination-out"
+    }
+
+    if (true) {
+        if (armCount == 0) {
+            ctx.arc(cx, cy, originalOuter, 0, Math.PI*2)
+        } else {
+            let points = []
+    
+            const bias = [0.1, 0.1, 0.2, 0.5, 1.0, 0.2, 0.3, 0.1, 0.5, 0.2, 0.3, 0.25, 0.1, 0.2, 0.05, 0.0, 0.2, 0.4, 0.1, 0.2, 0.4, 0.1, 0, 0.1, 0.2, 0, 0.1, 0]
+            const count = Math.abs(armCount)
+
+            for (let i = 0; i < count; i += 1) {
+                const outer = originalOuter
+                let inner = Math.max((Math.random()*1.5)*originalInner, bananaRadius*0.9)
+                if (inner > originalInner) {
+                    inner = Math.max((Math.random()*1.5)*originalInner, bananaRadius*0.9)
+                }
+        
+                const biasOffset = Math.round(Math.random()*100)
+                const selectedTipBias = bias[(biasOffset + i) % bias.length]
+                
+                dx = Math.sin(orbitAmount*(1 + 2*i) + selectedTipBias*Math.random()*Math.PI)
+                dy = Math.cos(orbitAmount*(1 + 2*i) + selectedTipBias*Math.random()*Math.PI)
+        
+                points.push([cx + dx*outer, cy + dy*outer])
+            
+                dx = Math.sin(orbitAmount*(2 + 2*i))
+                dy = Math.cos(orbitAmount*(2 + 2*i))
+        
+                points.push([cx + dx*inner, cy + dy*inner])
+            }
+        
+            ctx.moveTo(points[points.length - 1][0], points[points.length - 1][1])
+        
+            for (const point of points) {
+                ctx.lineTo(point[0], point[1])
+            }
+        
+            ctx.closePath()
+        }
+        ctx.fill()
+    }
+
+    ctx.globalCompositeOperation = "source-over"
+
+    const adjustTransform = new DOMMatrix([((bananaRadius*2)/image_size), 0, 0, ((bananaRadius*2)/image_size), radius/2, radius/2])
+
+    // ctx.setTransform(adjustTransform)
+    // ctx.transform(5.48632,0,0,5.13498,-495.997,-472.601)
+    // ctx.ellipse(188.833, 197.197, 98.427, 105.161, 0, 0, Math.PI*2)
+    // ctx.fill()
+
+    ctx.fillStyle = "#4da6ff"
+    ctx.strokeStyle="#4da6ff"
+    ctx.lineWidth = 10
+
+    const eyePosRandom = Math.random()
+    const thirdEyePosOffset = eyePosRandom*200 - 50
+
+    const thirdEyeRightControlX0 = 689.493
+    const thirdEyeRightControlY0 = 243.569
+    const thirdEyeRightControlX1 = 775.81
+    const thirdEyeRightControlY1 = 342.428
+
+    const thirdEyeRightControlX2 = 775.81
+    const thirdEyeRightControlY2 = 487.671
+
+    const thirdEyeRightX = 775.81 + thirdEyePosOffset
+    const thirdEyeRightY = 415.05
+
+    const thirdEyeLeftControlX0 = 644.229
+    const thirdEyeLeftControlY0 = 546.631
+    const thirdEyeLeftControlX1 = 512.648
+    const thirdEyeLeftControlY1 = 487.671
+
+    const thirdEyeLeftControlX2 = 512.648
+    const thirdEyeLeftControlY2 = 342.428
+
+    const thirdEyeLeftX = 512.648 - thirdEyePosOffset
+    const thirdEyeLeftY = 415.05
+
+    ctx.setTransform(adjustTransform)
+    ctx.transform(0.493992,0,0,1.18163,221.756,-196.862)
+    ctx.beginPath()
+    ctx.moveTo(645.632,243.569)
+    ctx.bezierCurveTo(thirdEyeRightControlX0, thirdEyeRightControlY0, thirdEyeRightControlX1,thirdEyeRightControlY1, thirdEyeRightX, thirdEyeRightY)
+    ctx.bezierCurveTo(thirdEyeRightControlX2, thirdEyeRightControlY2, 644.229,546.631, 644.229,546.631)
+    ctx.bezierCurveTo(thirdEyeLeftControlX0, thirdEyeLeftControlY0, thirdEyeLeftControlX1, thirdEyeLeftControlY1, thirdEyeLeftX, thirdEyeLeftY)
+    ctx.bezierCurveTo(thirdEyeLeftControlX2, thirdEyeLeftControlY2, 601.772,243.569, 645.632,243.569)
+    ctx.fill()
+
+    const eyePosOffset = eyePosRandom*200 - 100
+    const lashPosOffset = Math.random()*200 - 100
+
+    const eyeX = 462.916
+    const eyeY = 466.646 - eyePosOffset
+
+    const lashX = 230.837
+    const lashY = 420.23 - lashPosOffset
+
+    const ductX = 537.513
+    const ductY = 571.082
+
+    const midX = (ductX - eyeX)
+    const midY = (ductY - eyeY)
+    const len = Math.sqrt(midX*midX + midY*midY)
+    const dirX = midX / len
+    const dirY = midY / len
+    console.log(dirX, dirY)
+
+    const lashControlX0 = 420.919 + dirY*eyePosOffset/2
+    const lashControlY0 = 442.809 - dirX*eyePosOffset/2
+    const lashControlX1 = 245.756
+    const lashControlY1 = 408.626 - (lashPosOffset + eyePosOffset)/2
+    const lashControlX2 = 215.917
+    const lashControlY2 = 431.834 - (lashPosOffset + eyePosOffset)/2
+
+    const ductControlX0 = 519.279 + dirY*eyePosOffset/2
+    const ductControlY0 = 519.693 - dirX*eyePosOffset/2
+    const ductControlX1 = 497.756 + dirY*eyePosOffset/2
+    const ductControlY1 = 486.421 - dirX*eyePosOffset/2
+    const ductControlX2 = 540.023
+    const ductControlY2 = 578.156
+    const ductControlX3 = 499.635
+    const ductControlY3 = 579.951
+
+    const pathEye = () => {
+        ctx.beginPath()
+        ctx.moveTo(eyeX, eyeY)
+        ctx.bezierCurveTo(lashControlX0, lashControlY0, lashControlX1, lashControlY1, lashX, lashY)
+        ctx.bezierCurveTo(lashControlX2, lashControlY2, 276.664,605.099, 409.041,597.606)
+        ctx.bezierCurveTo(452.97,595.119, 473.181,580.805, 491.926,580.2)
+        ctx.bezierCurveTo(ductControlX3,ductControlY3, ductControlX2,ductControlY2, ductX,ductY)
+        ctx.bezierCurveTo(ductControlX0, ductControlY0, ductControlX1, ductControlY1, eyeX, eyeY)
+    }
+
+
+    // left eye
+    ctx.setTransform(adjustTransform)
+    ctx.transform(1,0,0,1,-74.5545,32.1319)
+    pathEye()
+    ctx.fill()
+
+    // right eye
+    ctx.setTransform(adjustTransform)
+    ctx.transform(-1,0,0,1,1154.55,32.1319)
+    pathEye()
+    ctx.fill()
+
+    // mouth half 1
+    ctx.setTransform(adjustTransform)
+
+    ctx.transform(1,0,0,1,2.48067,20.9764)
+    ctx.transform(1,0,0,1,19.8926,-8.28856)
+
+    ctx.beginPath()
+    ctx.moveTo(518.45,931.635)
+    ctx.bezierCurveTo(577.672,772.559, 602.993,767.521, 602.993,767.521)
+    ctx.lineTo(681.734,870.299)
+    ctx.bezierCurveTo(753.961,797.232, 816.176,741.785, 815.18,704.528)
+
+    ctx.stroke()
+
+    // mouth half 2
+    ctx.setTransform(adjustTransform)
+
+    ctx.transform(1,0,0,1,2.48067,20.9764)
+    ctx.transform(-1,0,0,1,1055.15,-8.28856)
+    
+    ctx.beginPath()
+    ctx.moveTo(518.45,931.635)
+    ctx.bezierCurveTo(577.672,772.559, 602.993,767.521, 602.993,767.521)
+    ctx.lineTo(681.734,870.299)
+    ctx.bezierCurveTo(753.961,797.232, 816.176,741.785, 815.18,704.528)
+
+    ctx.stroke()
+
+    // nose
+    ctx.setTransform(adjustTransform)
+    ctx.transform(1,0,0,1,-31.6002,36.9996)
+
+    ctx.beginPath()
+    ctx.moveTo(571.6,626.086)
+    ctx.lineTo(610.453,649.915)
+    ctx.lineTo(532.748,649.915)
+    ctx.lineTo(571.6,626.086)
+    ctx.fill()
+
+    // ctx.drawImage(image, radius/2, radius/2, bananaRadius*2, bananaRadius*2)
+
+    return canvas.toBuffer("image/png")
 }
 
 function reloadStarLines() {
